9489258b22370dbfdc09d5df18ca939bf382b89e
Geraldina (laptop) <grushevskaya.d.v@yandex.ru>
2020-04-25 13:56:55 +0300

    Fix counter_id

--- server/counter_id.py
+++ server/counter_id.py
@@ -1,9 +1,13 @@
 """ Автоинкрементный идентификатор MongoDB """
+from bson import ObjectId
+
 from . import MONGO
+
 
 def get_next_id(collection_name):
     """ Получить следующий идентификатор """
     result = MONGO.db.counter.find_one_and_update(filter={"_id": collection_name},
                                                   update={"$inc": {"count": 1}},
                                                   upsert=True, new=True)
-    return result["count"]
+    # ObjectId - 24-character hex string
+    return ObjectId(str(result["count"]).zfill(24))
