d6b86ac216b48f1fe460cd3ccf4f07fec9b53ac1
Geraldina (laptop) <grushevskaya.d.v@yandex.ru>
2020-04-26 12:35:06 +0300

    Rename data_bd to data_db

--- server/__init__.py
+++ server/__init__.py
@@ -12,13 +12,13 @@ APP.config["JSON_AS_ASCII"] = False
 APP.config["MONGO_URI"] = os.environ["MONGO_URI"]
 MONGO = PyMongo(APP)
 
-from . import data_bd
+from . import data_db
 
 if os.environ.get("FLASK_ENV") == "development" and MONGO.db.event.count_documents({}) == 0:
-    data_bd.add_event1()
-    data_bd.add_event2()
-    data_bd.add_event3()
-    data_bd.add_mock_events(7)
+    data_db.add_event1()
+    data_db.add_event2()
+    data_db.add_event3()
+    data_db.add_mock_events(7)
 
 from . import views
 from .api import list_events, booking
