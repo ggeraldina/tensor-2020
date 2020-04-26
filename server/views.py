a44cb9bc40fe2b0634fe0727617242b92f8a617d
Geraldina (laptop) <grushevskaya.d.v@yandex.ru>
2020-04-26 12:01:07 +0300

    Add mock-pages

--- server/views.py
+++ server/views.py
@@ -9,3 +9,13 @@ from . import APP
 def index():
     """ Главная страница """
     return send_from_directory(APP.static_folder, 'index.html')
+
+@APP.route("/list_events")
+def list_events():
+    """ Mock list events GET """
+    return send_from_directory(APP.static_folder, 'mock_pages/list_events_get_request.html')
+
+@APP.route("/booking")
+def booking():
+    """ Mock booking POST """
+    return send_from_directory(APP.static_folder, 'mock_pages/booking_post_request.html')
