8453640efd0c338c7ed21b33d8fc16d3805f0339
ElizavetaKoroleva <43936760+ElizavetaKoroleva@users.noreply.github.com>
2020-05-24 20:10:55 +0300

    Update cardList.ts

--- client/src/api/cardList.ts
+++ client/src/api/cardList.ts
@@ -2,14 +2,12 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { TCardList } from '../types';
 
-export const baseUrl = process.env.BASE_URL ? process.env.BASE_URL : 'http://localhost:5000';
-
 export const fetchCardList = createAsyncThunk(
   'cardList/fetch',
   async (params: {limit: number, offset: number}) => {
     const response = await axios({
       method: 'GET',
-      url: `${baseUrl}/api/v1/get_events_list`,
+      url: `https://tensor-2020-test.herokuapp.com/api/v1/get_events_list`,
       params: {
         limit: params.limit,
         offset: params.offset
