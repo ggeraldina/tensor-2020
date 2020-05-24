b1cf3989138de6d0434b0742593de482a7774a65
Elizaveta Koroleva <koroleva@aeroidea.ru>
2020-05-24 18:38:01 +0300

    Интеграция

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -4,7 +4,7 @@ import Link from 'react-redux';
 
 const Card: React.SFC<TCard> = (props): JSX.Element => {
   const { photo, title, start_time} = props;
-  const [date, time] = start_time.split(" ");
+  const [date, time] = start_time.split("T");
   let fullDate = new Date(date);
   const months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 
                   'июля', 'авгуса', 'сентября', 'октября', 'ноября', 'декабря'];
@@ -15,7 +15,7 @@ const Card: React.SFC<TCard> = (props): JSX.Element => {
       <div className="card__description">
         <h2 className="card__title">{title}</h2>
         <span className="card__time">
-          {fullDate.getDate() + months[fullDate.getMonth()]}, {time.slice(0, 5)}
+          {`${fullDate.getDate()} ${months[fullDate.getMonth()]}`}, {time.slice(0, 5)}
         </span>
         <a className="card__button" href="#">
           Забронировать
