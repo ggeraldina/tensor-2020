6fd4ddb0f7191ead0bca70cb0454bf06c0e0b051
Elizaveta Koroleva <koroleva@aeroidea.ru>
2020-06-03 13:17:59 +0300

    relative url

--- client/src/api/initialCardList.ts
+++ client/src/api/initialCardList.ts
@@ -7,7 +7,7 @@ export const fetchInitialCardList = createAsyncThunk(
   async () => {
     const response = await axios({
       method: 'GET',
-      url: `https://tensor-2020-test.herokuapp.com/api/v1/get_events_list`,
+      url: `/api/v1/get_events_list`,
       params: {
         limit: 6,
         offset: 0
