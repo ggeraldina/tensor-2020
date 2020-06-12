d7e9270a0635c67f9de018692874cd935de39630
Elizaveta Koroleva <koroleva@aeroidea.ru>
2020-06-12 13:43:17 +0300

    fix

--- client/src/api/initialCardList.ts
+++ client/src/api/initialCardList.ts
@@ -1,18 +1,18 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { TCardList } from '../types';
+import { IEventList } from '../types';
 
 export const fetchInitialCardList = createAsyncThunk(
   'initialCardList/fetch',
   async () => {
     const response = await axios({
       method: 'GET',
-      url: `/api/v1/get_events_list`,
+      url: `http://tensor-2020-test.herokuapp.com/api/v1/get_events_list`,
       params: {
         limit: 6,
         offset: 0
       }
     });
-    return response.data as TCardList;
+    return response.data as IEventList;
   }
 );
