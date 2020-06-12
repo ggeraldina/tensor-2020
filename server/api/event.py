0359332fb460b189efb49c101e0aa59970ab4e80
Geraldina (laptop) <grushevskaya.d.v@yandex.ru>
2020-06-12 22:41:42 +0300

    Fix

--- server/api/event.py
+++ server/api/event.py
@@ -1,5 +1,7 @@
 """ Мероприятие """
 from flask import jsonify, request
+from werkzeug.exceptions import NotFound
+
 
 from server import APP, MONGO
 from server.exception.error_data_db import ErrorDataDB
@@ -15,7 +17,10 @@ def get_event(version):
         event_id = parse_object_id(request.args.get("id"))
     except ErrorDataDB as error_db:
         return jsonify({"message": error_db.message, "event": {}, "tickets": []})
-    event = MONGO.db.event.find_one_or_404({"_id": event_id})
+    try:
+        event = MONGO.db.event.find_one_or_404({"_id": event_id})    
+    except NotFound:
+        return jsonify({"message": f"Нет такого события {event_id}", "event": {}, "tickets": []})
     tickets = list(MONGO.db.ticket.find({"event": event_id}, {"event": 0}))
     event["id"] = event.pop("_id")
     for ticket in tickets:
