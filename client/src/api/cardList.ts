8038fc76f9731cd3485f33546c667b685f8eb629
Geraldina (laptop) <grushevskaya.d.v@yandex.ru>
2020-06-12 14:21:47 +0300

    Add a environment variables REACT_APP_ with a url

--- client/src/api/cardList.ts
+++ client/src/api/cardList.ts
@@ -2,12 +2,15 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { IEventList } from '../types';
 
+export const baseUrl = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : '';
+console.log("baseUrl ", baseUrl)
+
 export const fetchCardList = createAsyncThunk(
   'cardList/fetch',
-  async (params: {limit: number, offset: number}) => {
+  async (params: { limit: number, offset: number }) => {
     const response = await axios({
       method: 'GET',
-      url: `/api/v1/get_events_list`,
+      url: `${baseUrl}/api/v1/get_events_list`,
       params: {
         limit: params.limit,
         offset: params.offset
